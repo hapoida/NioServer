670c7be979dbd2e4ab79318ffa694c199607f1b9
hapoida <hapoida@sk.com>
2013-09-09 10:20:27 +0900

    해봉작업 주석추가

--- NIO/src/main/java/com/benepia/card/bc/BcCardService.java
+++ NIO/src/main/java/com/benepia/card/bc/BcCardService.java
@@ -7,6 +7,7 @@ public class BcCardService extends CardServerAdapter{
 	@Override
 	public String requestToCardServer(String msg) {
 		// TO-DO BC카드사에 요청을 진행할 내용 작업
+		//해봉작업
 		return (msg + "를 BC카드에 요청 진행 후 완료");
 	}
 	
